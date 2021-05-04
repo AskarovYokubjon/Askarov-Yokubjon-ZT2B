7fab90017ca5eb9ac682ef07b6a51a614e90b4c8
Yokubjon Askarov <yaqub200211@gmail.com>
2021-05-04 11:29:57 +0200

    Hi

---
+++ newEmptyPHP.php
@@ -0,0 +1,138 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>NJR11</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body{
+				background: black;
+			}
+			.h2{
+				text-align: center;
+				text-decoration: underline;
+			}
+		</style>
+	</head>
+	<body style="color: rgb(10, 20, 60)">
+		<h1 style="color: rgb(10, 20, 60); text-align: center; ">NJR11</h1><hr style="border-width: 0.3px; border-color: rgb(10, 20, 60)">
+		<p>Neymar da Silva Santos Júnior, či krátce Neymar. Je <i>Brazilský fotbalista</i>. Hraje jako útočník ve francouzském ligovém klubu <i>Paris Saint-Germain FC</i> a v brazilském národním týmu. Je považován za jednoho z nejtalentovanějších fotbalistů nejen v Brazílii, ale i ve světě. Vyniká vytříbenou technikou a rychlostí. Účastník <i>mistrovství světa 2014</i> a <i>2018</i>. <br><br>
+
+		Dne 29. prosince 2010 obsadil třetí místo za Andrésem D'Alessandrem a <i>Juanem Sebastiánem Verónem</i> v anketě <i>Fotbalista roku Jižní Ameriky</i>, v roce 2011 a 2012 již tuto anketu ovládl. Za rok 2011 vyhrál cenu FIFA Puskás Award o nejhezčí gól roku. Je známý svým zrychlením, rychlostí, kličkováním, zakončením a technickými schopnostmi. Díky svému stylu hraní je kritiky, médii a fanoušky srovnáván s <i>Lionelem Messim</i> a brazilskou legendou Pelém, který sám označil Neymara za "nejlepšího fotbalistu na světě".</p><br>
+
+		<h2 class="h2">Klubová kariéra</h2>
+		<h3>Začátky</h3>
+		<p>Narodil se v Mogi Das Cruzes v São Paulu. V roce 1992 se s rodinou přestěhoval do Sao Vicente, pak v roce 2003 do Santosu, kde začal hrát v témže roce fotbal za Santos FC. V sedmnácti podepsal plnou profesionální smlouvu se Santos FC.</p><br>
+
+		<h3>Santos FC</h3>
+		<p>Za Santos FC debutoval dne 7. března 2009 ve věku 17 let. Následující týden vstřelil svůj první gól za Santos proti Mogi Mirim. O měsíc později, 11. dubna, vstřelil gól proti Palmeiras na Campeonato Paulista a rozhodl tak o postupu do semifinále. Ve finále však Santos utrpěl porážku 4-2. Ve své první sezóně dal 14 gólů v 48 zápasech. Stal se nejlepším hráčem v roce 2010 a také nejlepším střelcem v roce 2010. <br><br>
+
+        Neymar vstřelil celkem 6 gólů na Copa Libertadores 2011 a byl tak třetí nejlepší střelec. Ve finále proti uruguayskému týmu CA Peñarol dal jeden gól a Santos zvítězil 2:1. Výhra přinesla Santosu jejich první triumf v Poháru osvoboditelů od roku 1963, kdy hrála za klub brazilská legenda Pelé. <br><br>
+
+		Dne 5. února 2012 vstřelil svůj stý gól jako profesionální fotbalista v zápase na Campeonato Paulista proti Palmeiras. Dne 7. března 2012 byl nejlepší hráč proti Internacionalu, kde Santos vyhrál 3-1. Trenér Mano Menezes řekl, že věří, že Neymar získá větší respekt a že je důležité, aby šel hrát do Evropy. 26. května 2013 Neymar oznámil, že podepíše pětiletou smlouvu se španělským velkoklubem FC Barcelona, přičemž zvažoval ještě nabídku z Realu Madrid. V Santosu měl smlouvu platnou do roku 2014, klub ale souhlasil s jeho předčasným odchodem.</p><br>
+
+		<h3>FC Barcelona</h3>
+		<p>V dresu FC Barcelona debutoval 30. července 2013 v přátelském utkání v PGE Areně v Gdaňsku proti domácímu týmu Lechia Gdańsk. Nastoupil 12 minut před koncem řádné hrací doby, střetnutí skončilo remízou 2:2. V La Lize debutoval 18. srpna 2013 v prvním kole proti Levante UD, do zápasu nastoupil v 63. minutě. Barcelona deklasovala soupeře 7:0. První gól vstřelil 21. srpna 2013 v Supercopa de España, kde vyrovnával na konečných 1:1 s Atléticem Madrid. S klubem tuto trofej vyhrál. 24. září 2013 vstřelil svůj první ligový gól (ve svém šestém zápase) proti Realu Valladolid, když v 5. minutě otevíral skóre utkání, které skončilo vítězstvím Barcelony 4:1. <br><br>
+
+		S Barcelonou postoupil ze základní skupiny H do vyřazovacích bojů Ligy mistrů 2013/14, katalánský tým v ní obsadil první místo. V posledním utkání skupiny 11. prosince 2013 vstřelil hattrick proti skotskému týmu Celtic FC a přispěl tak k drtivému vítězství 6:1. Byl to jeho premiérový hattrick v dresu Barcelony a zároveň se poprvé střelecky prosadil v evropské Lize mistrů. O necelý týden později v 16. kole La Ligy sezóny 2013/14 zařídil dvěma góly výhru 2:1 nad Villarealem, dokázal tak opět střelecky nahradit zraněného klíčového hráče Barcelony Argentince Lionela Messiho.</p><br>
+		<img src="../Skola/Neymar2.jfif">
+
+		<h4>Přestup do Barcelony</h4>
+		<p>V lednu 2014 se objevily informace, že Barcelona mohla za Neymarův přestup zaplatit až 95 milionů eur (místo oficiálně zveřejněných 57 milionů), což by byl nejdražší transfer v historii fotbalu. Případ řešil španělský soud, který obdržel žalobu od zaměstnance Barcelony na klubového prezidenta Sandro Rosella za netransparentnost přestupu a zpronevěru. Rosell krátce nato odstoupil z funkce. Barcelona nakonec se svolením Neymarova otce Neymara Silvy Santose (který je i jeho agentem) zveřejnila účty ohledně přestupu Neymara. Brazilský klub Santos FC podle nich dostal z 57 milionů eur částku 17,1 milionu eur, zbytek dostala společnost, kterou vlastní hráčovi rodiče. Smlouva obsahuje i klazuli o vyplacení dalších 2 milionů eur, pokud se Neymar dostane během pěti let mezi finalisty Zlatého míče. Neymarův plat činí 56,7 milionu eur (za 5 let platnosti smlouvy).</p><br>
+
+		<h3>Paris Saint-Germain FC</h3>
+		<p>3. srpna 2017 byl potvrzen již nějakou dobu avizovaný přestup brazilského útočníka do francouzského klubu Paris Saint-Germain vlastněného bohatými katarskými majiteli. PSG využil vysoké klauzule ve smlouvě hráče, která činila 222 milionů eur (více než 6 miliard Kč k datu). Tento transfer překonal více než dvojnásobně hodnotu do té doby nejdražšího hráče, Francouze Paula Pogby, jenž přestoupil v srpnu 2016 z Juventusu do Manchesteru United za 110 milionů eur.</p><br><br>
+
+		<a href="https://www.google.cz/search?q=neymar&sxsrf=ALeKk03EZHyvXNqYd1zm4YShNy9ZeJPlIA:1585511074685&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjvo5ODucDoAhUQTsAKHTlVBYkQ_AUoAXoECBoQAw#imgrc=Sj_muHIkqgR1pM"><img src="../Skola/Neymar1.jpeg"></a>
+
+		<h2 class="h2">Úspěchy</h2>
+		<h3>Individuální</h3>
+		<ul><li>1× nejlepší mladý hráč Campeonato Paulista (2009)</li>
+			<li>2× nejlepší útočník Campeonato Paulista (2010, 2011)</li>
+			<li>2× nejlepší hráč Campeonato Paulista (2010, 2011)</li>
+			<li>1× nejlepší hráč Santander Libertadores poháru (2011)</li>
+			<li>2× cena Squad Crack Brasileirão mistrovství (2010, 2011)</li>
+			<li>1× nejlepší hráč v brazilské lize, uděleno časopisem Placar (Zlatý míč) (2011)</li>
+			<li>2× Stříbrný míč, nejlepší útočník v brazilské lize (2010, 2011)<li>
+			<li>1× nejlepší střelec Copa do Brazil (2010)</li>
+			<li>1× FIFA Puskás Award (2011)</li>
+			<li>2× fotbalista roku Jižní Ameriky (2011, 2012)</li>
+			<li>1× Zlatý míč (Adidas Golden Ball) pro nejlepšího hráče Konfederačního poháru FIFA 2013</li></ul>
+
+		<h3>Reprezentace</h3>
+		<ol><li>2009 Brazílie U17</li>
+			<li>2011 Brazílie U20</li>
+			<li>2012 Brazílie U23</li>
+			<li>2010 Brazílie</li></ol><br><br>
+
+
+
+
+
+		<table border="1">
+			<th>Přesné datumy zápasů tohohle roku</th>
+			<tr rowspan="2"><td>Zápasy roku 2020</td></tr>
+			<tr>
+				<td>23.5.</td>
+				<td>16.5.</td>
+				<td>9.5.</td>
+				<td>2.5.</td>
+				<td>26.4.</td>
+				<td>25.4.</td>
+				<td>18.4.</td>
+				<td>11.4.</td>
+				<td>4.3.</td>
+				<td>29.2.</td>
+			</tr>
+			<tr>
+				<td>23.2.</td>
+				<td>18.2.</td>
+				<td>15.2.</td>
+				<td>12.2.</td>
+				<td>9.2.</td>
+				<td>4.2.</td>
+				<td>1.2.</td>
+				<td>29.1.</td>
+				<td>26.1.</td>
+				<td>22.1.</td>
+			</tr>
+		</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<form>
+			<label>Zadej jméno:</label>
+			<input type="text" name="text"><br><br>
+
+			<label>Zadej heslo:</label>
+			<input type="password" name="heslo"><br><br>
+
+			<label>Který hráč se ti líbí nejvíc?:</label>
+			<input type="select" name="nastav"><br><br>
+
+			<label>Líbí se ti Neymar?:</label>
+			<input type="radio" name="vyber">ANO
+			<input type="radio" name="vyber">NE
+			<input type="radio" name="vyber">MOŽNÁ <br><br>
+
+			<label>Napiš svůj názor o Neymarovi:</label>
+			<textarea></textarea>
+			<input type="submit" name="poslat" value="Poslat"><br><br>
+
+			<a href="https://cs.wikipedia.org/wiki/Neymar"><button>Odkaz</button></a>
+
+
+		</form>
+	</body>
+</html>
